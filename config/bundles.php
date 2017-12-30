04732d4e4e10e34ceddd0ed00f322b4909e70157
fennore <29664945+fennore@users.noreply.github.com>
2017-12-31 00:29:57 +0100

    Install vich uploader bundle

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
