84b1d601d25669cf83daa61679ae054a7d02bbda
fennore <29664945+fennore@users.noreply.github.com>
2020-09-14 01:22:25 +0200

    Update tests for RouteController

--- tests/Controller/Api/RouteControllerTest.php
+++ tests/Controller/Api/RouteControllerTest.php
@@ -1,28 +1,74 @@
 <?php
 
-namespace App\Tests\Controller;
+namespace App\Tests\Api\Controller;
 
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class ApiControllerTest extends WebTestCase
+class RouteControllerTest extends WebTestCase
 {
+    private const TEST_STAGE = 3;
+
+    public function testEncodedRoute()
+    {
+        $client = $this->getTestClient();
+        $client->request('GET', 'api/encoded-route');
+        $this->assertHalJson($client);
+    }
+
     public function testGetLocations()
     {
-        $client = static::createClient();
+        $client = $this->getTestClient();
 
-        $client->request('GET', 'api/route/1');
-        
-        // Assert status code
-        // $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        // Assert that the "Content-Type" header is "application/hal+json"
-        /*$this->assertTrue(
-            $client->getResponse()->headers->contains(
-                'Content-Type',
-                'application/json'
-            ),
-            'the "Content-Type" header is "application/json"' // optional message shown on success
-        );*/
-        // Assert response structure
-        //...
+        $client->request('GET', 'api/route/'.self::TEST_STAGE);
+        $this->assertHalJson($client);
+
+        $client->request('GET', 'api/route/bla');
+        $this->assertTrue(
+            $client->getResponse()->isNotFound()
+        );
+    }
+
+    public function testUpdateLocations()
+    {
+        $client = $this->getTestClient();
+
+        $client->request('PUT', 'api/route/'.self::TEST_STAGE);
+        $this->assertTrue(
+            $client->getResponse()->isEmpty()
+        );
+        $client->request('PUT', 'api/route/bla');
+        $this->assertTrue(
+            $client->getResponse()->isNotFound()
+        );
+    }
+
+    public function testClearRoute()
+    {
+        $client = $this->getTestClient();
+
+        $client->request('DEL', 'api/route/0');
+        $this->assertTrue(
+            $client->getResponse()->isNotFound()
+        );
+        $client->request('DEL', 'api/route/all');
+        $this->assertTrue(
+            $client->getResponse()->isEmpty()
+        );
+    }
+
+    private function assertHalJson(KernelBrowser $client): void
+    {
+        $this->assertTrue(
+            $client->getResponse()->isOK()
+        );
+        $this->assertTrue(
+            $client->getResponse()->headers->contains('Content-Type', 'application/hal+json')
+        );
+    }
+
+    private function getTestClient(): KernelBrowser
+    {
+        return static::createClient(['environment' => 'test']);
     }
 }
