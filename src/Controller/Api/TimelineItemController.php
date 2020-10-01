322818bea1db59174f18092c9730322b97db42c8
fennore <29664945+fennore@users.noreply.github.com>
2020-10-01 06:55:39 +0200

    Add create and update methods to TimelineItemController and fix responses

--- src/Controller/Api/TimelineItemController.php
+++ src/Controller/Api/TimelineItemController.php
@@ -35,7 +35,49 @@ class TimelineItemController extends AbstractController
 
         $json = $serializer->serialize($pager, 'json');
 
-        return JsonResponse::fromJsonString($json);
+        return JsonResponse::fromJsonString(
+            $json,
+            Response::HTTP_OK,
+            ['Content-Type' => 'application/hal+json']
+        );
+    }
+
+    /**
+     * Create the new TimelineItems.
+     *
+     * @Route("/api/images", name="api_timelineitems_create", methods={"POST"})
+     */
+    public function createTimelineItems(
+        Request $request,
+        TimelineItemRepository $itemRepo,
+        SerializerInterface $serializer): Response
+    {
+        $items = $serializer->deserialize($request->getContent(), 'array<App\Entity\Item\TimelineItem>', 'json');
+
+        foreach ($items as $item) {
+            $itemRepo->createTimelineItem($item);
+        }
+
+        return new JsonResponse(null, 201);
+    }
+
+    /**
+     * Update the TimelineItems using given identifier.
+     *
+     * @Route("/api/images", name="api_timelineitems_update", methods={"PUT"})
+     */
+    public function updateTimelineItems(
+        Request $request,
+        TimelineItemRepository $itemRepo,
+        SerializerInterface $serializer): Response
+    {
+        $items = $serializer->deserialize($request->getContent(), 'array<App\Entity\Item\TimelineItem>', 'json');
+
+        if (is_array($items) && count($item) > 0) {
+            $itemRepo->updateTimelineItems($item);
+        }
+
+        return new JsonResponse([], 204);
     }
 
     /**
@@ -55,7 +97,7 @@ class TimelineItemController extends AbstractController
         }
         // remove the images with given id
         // - optionally also physically remove the image from the server
-        return new JsonResponse(null, 204);
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
     /**
@@ -67,6 +109,6 @@ class TimelineItemController extends AbstractController
     {
         // empty images list
         // reset images state
-        return new JsonResponse(null, 204);
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
