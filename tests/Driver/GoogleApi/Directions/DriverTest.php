a0acc21f751105b3ba73e373061b07a9a58cb99a
fennore <29664945+fennore@users.noreply.github.com>
2020-12-26 16:12:23 +0100

    Add test files

---
+++ tests/Driver/GoogleApi/Directions/DriverTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Tests\Driver\GoogleApi\Directions;
+
+use PHPUnit\Framework\TestCase;
+
+class DriverTest extends TestCase
+{
+}
---
+++ tests/Driver/GoogleApi/Directions/ResponseTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Tests\Driver\GoogleApi\Directions;
+
+use PHPUnit\Framework\TestCase;
+
+class ResponseTest extends TestCase
+{
+}
